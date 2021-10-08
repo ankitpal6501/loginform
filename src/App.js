196407adfda9d0cd443bc94ac8268848ef350566
unknown <ankitpal6501@gmail.com>
2021-10-08 22:44:19 +0530

    pal branch

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
-      <h2>hello everyone</h2>
+      <h1>hello everyone i am hear</h1>
+      <h2>hello everyone i am hear</h2>
+      <h3>hello everyone i am hear</h3>
+      <h4>hello everyone i am hear</h4>
+      <h5>hello everyone i am hear</h5>
+      <h6>hello everyone i am hear</h6>
     </div>
   );
 }
